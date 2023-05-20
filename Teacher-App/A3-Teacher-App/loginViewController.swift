1b7ef51342607db80fbc96ffea5755eea9847740
WZF888888 <100253117+WZF888888@users.noreply.github.com>
2023-05-20 21:08:47 +1000

    Update

---
+++ Teacher-App/A3-Teacher-App/loginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  loginViewController.swift
+//  A3-Teacher-App
+//
+//  Created by John Wang on 2023/5/20.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+}
+
