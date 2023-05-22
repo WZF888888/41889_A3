a6a686392a19df864fcee56f053630dca48c43d0
WZF888888 <100253117+WZF888888@users.noreply.github.com>
2023-05-23 01:07:51 +1000

    Updates

---
+++ Teacher-App/A3-Teacher-App/createAssignmentViewController.swift
@@ -0,0 +1,17 @@
+//
+//  createAssignmentViewController.swift
+//  A3-Teacher-App
+//
+//  Created by John Wang on 2023/5/23.
+//
+
+import UIKit
+import Firebase
+
+class createAssignmentViewController: UIViewController {
+    let uesrEmail:String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Teacher-App/A3-Teacher-App/loginViewController.swift
+++ Teacher-App/A3-Teacher-App/loginViewController.swift
@@ -27,7 +27,7 @@ class loginViewController: UIViewController {
     
     @IBAction func handleEmailNoInput(_ sender: Any) {
         if userEmailTextfield.text == "" {
-            errorLabel1.text = "Please Enter the email address in text field"
+            errorLabel1.text = "Please Enter the email"
             errorLabel1.textColor = UIColor.red
             inputTureForUsername = false
         } else {
@@ -39,7 +39,7 @@ class loginViewController: UIViewController {
     
     @IBAction func handlePasswordNoInput(_ sender: Any) {
         if passwordInputTextField.text == "" {
-            errorLabel2.text = "Please Enter the password in text field"
+            errorLabel2.text = "Please Enter the password"
             errorLabel2.textColor = UIColor.red
             inputTureForPassword = false
         } else {
@@ -49,12 +49,16 @@ class loginViewController: UIViewController {
         controlButton(inputTureForUsername: inputTureForUsername, inputTureForPassword: inputTureForPassword)
     }
     
-    func controlButton(inputTureForUsername:Bool, inputTureForPassword: Bool){
+    func controlButton(inputTureForUsername:Bool, inputTureForPassword: Bool) {
         if inputTureForUsername == true && inputTureForPassword == true {
             loginButton.isEnabled = true
         } else {
             loginButton.isEnabled = false
         }
+    }
+    
+    func passEmail(){
+        
     }
     
     @IBAction func handleLoginClick(_ sender: Any) {
@@ -63,7 +67,7 @@ class loginViewController: UIViewController {
         // Using Firebase
         Auth.auth().signIn(withEmail: finalUsername, password: finalPassword){ [weak self] (authResult, error) in
             if let error = error {
-                // Handle login error
+                // Handle login error and display the error message when the login was not ok.
                 self!.systemErrorLabel.text = "Email or Password Not Correct!"
                 self!.systemErrorLabel.textColor = UIColor.red
                 print("Login error: \(error.localizedDescription)")
@@ -72,6 +76,8 @@ class loginViewController: UIViewController {
                 let storyboard = UIStoryboard(name: "Main", bundle: nil)
                 let MenuVC = storyboard.instantiateViewController(withIdentifier: "TeacherMenu")
                 self?.present(MenuVC, animated: true, completion: nil)
+                // Also Passing user email address to different controller
+                self!.passEmail()
             }
         }
     }
