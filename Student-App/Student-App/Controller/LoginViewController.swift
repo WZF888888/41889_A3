a08e904701e1e354c4b812148e8cff56ea17acd5
YeseulShin0 <Yeseul.Shin-1@student.uts.edu.au>
2023-05-23 22:20:15 +1000

    Update loginviewcontroller

--- Student-App/Student-App/Controller/LoginViewController.swift
+++ Student-App/Student-App/Controller/LoginViewController.swift
@@ -15,27 +15,47 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     
     @IBAction func loginButtonTapped(_ sender: Any) {
-        let studentEmail = studentEmailTextField.text ?? ""
-        let password = passwordTextField.text ?? ""
-       
+        guard let studentEmail = studentEmailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
         
-        Auth.auth().signIn(withEmail: studentEmail, password: password){ [weak self] (authResult, error) in
-                    if let error = error {
-                        // Handle login error and display the error message when the login was not ok
-                    } else {
-                        // Login successful, Jump to the teacher app menu.
-                     
-                    }
-                }
-        
+        Auth.auth().signIn(withEmail: studentEmail, password: password) { [weak self] (authResult, error) in
+            if let error = error {
+                // Handle login error and display the error message in the login view
+                self?.displayAlert(withTitle: "Login Error", message: "Incorrect Email or Password.", completion: {
+                    // Navigate to the LoginViewController in the storyboard
+                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+                    self?.navigationController?.setViewControllers([loginVC], animated: true)})
+            } else {
+                // Login successful, jump to the student scene
+                self?.displayAlert(withTitle: "Login Successful", message: "", completion: {})
+            }
+        }
+    }
+
+    func displayAlert(withTitle title: String, message: String, completion: (() -> Void)?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            completion?()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
     
+    @IBAction func forgotButtonTapped(_ sender: Any) {
+        let alertTitle = "Forgot your password?"
+        let alertMessage = "Please contact IT Support or visit UTS Student Centre."
+        
+        let alertController = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    
-    
     
 }
