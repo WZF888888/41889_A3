67275c2c453c5151f1b1eba250fca42b24e9e1f3
YeseulShin0 <Yeseul.Shin-1@student.uts.edu.au>
2023-05-24 13:38:36 +1000

    Update studentviewcontroller

--- Student-App/Student-App/Controller/StudentViewController.swift
+++ Student-App/Student-App/Controller/StudentViewController.swift
@@ -10,10 +10,57 @@ import UIKit
 
 class StudentViewController: UIViewController {
 
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+
+    var email: String?
+    var sessionTimer: Timer?
+    var remainingTime: TimeInterval = 60 * 5 // Set the desired session timeout duration in seconds
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        emailLabel.text = email
+        
+        // Set the current date in the date label
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let currentDate = Date()
+        let dateString = dateFormatter.string(from: currentDate)
+        dateLabel.text = dateString
+        
+        startSessionTimer()
     }
 
+    func startSessionTimer() {
+        sessionTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        updateTimer() // Update the initial timer display
+    }
+
+    @objc func updateTimer() {
+        remainingTime -= 1
+        timerLabel.text = timeFormatted(remainingTime)
+        
+        if remainingTime == 0 {
+            logout()
+        }
+    }
+
+    func logout() {
+        sessionTimer?.invalidate()
+        
+        // sign out the student and navigate back to the login screen
+        if let loginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController {
+            navigationController?.setViewControllers([loginVC], animated: true)
+        }
+    }
+
+    // remaining time in mm:ss format
+    func timeFormatted(_ totalSeconds: TimeInterval) -> String {
+        let minutes = Int(totalSeconds) / 60
+        let seconds = Int(totalSeconds) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
 
 }
